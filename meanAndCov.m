40f238ab3c9e7fc31c99b0f576419b5f60eab77e
ManuGS2 <manu.skate17@gmail.com>
2019-09-25 12:24:41 -0500

    Training

--- meanAndCov.m
+++ meanAndCov.m
@@ -1,36 +1,38 @@
 
 function [mean, cov] = meanAndCov(imageMatrix)
 col = size(imageMatrix,2);
-row = size(imageMatrix,1);
+row = size(imageMatrix,1)/3; % There 3 images in a column
+muestras = col*row*3;
 mean = zeros(3,1);
-cov = zeros(3:3);
-pixelValue = zeros(3:1);
+cov = zeros(3,3);
+pixelValue = zeros(3,1);
 
 % mean
 for i=1:3 % image
-    for x=1:row % row
+    for x=1:row/3 % row
         for y=1:col % col
-            if(imageMatrix(row*(s-1)+x, y) ~= 0)
-                mean(1) = mean(1) + imageMatrix(row*(s-1)+x, y);
+            if(imageMatrix(row*(i-1)+x, y) ~= 0)
+                mean(1) = mean(1) + imageMatrix(row*(i-1)+x, y);
                 mean(2) = mean(2) + x;
                 mean(3) = mean(3) + y;
             end
         end
     end
 end
+mean = mean/muestras;
 
 % cov
 for i=1:3 % image
-    for x=1:row % row 
+    for x=1:row/3 % row 
         for y=1:col %col
             if(imageMatrix(row*(i-1)+x, y) ~= 0)
                 pixelValue(1) = imageMatrix(row*(i-1)+x, y);
                 pixelValue(2) = x;
                 pixelValue(3) = y;
-                cov = cov * ( pixelValue - mean)*(pixelValue - mean)';
+                cov = cov + (pixelValue - mean)*(pixelValue - mean)';
             end
         end
     end
 end
-cov = cov/col*row;
+cov = cov/muestras;
 end
---
+++ training.m
@@ -0,0 +1,39 @@
+
+function [covHalo, meanHalo, probaHalo,...
+       covPros, meanPros, probaPros,...
+       covFondo, meanFondo, probaFondo] = training(entre1, entre2, entre3)
+
+% Calculamos las probabilidades de cada clase
+% a partir de la máscara obtenida de cada entrenamiento
+Halo1 = getMask(entre1);
+Halo2 = getMask(entre2);
+Halo3 = getMask(entre3);
+probaHalo = classProb(Halo1,Halo2,Halo3);
+
+Pros1 = getMask(entre1);
+Pros2 = getMask(entre2);
+Pros3 = getMask(entre3);
+probaPros = classProb(Pros1,Pros2,Pros3);
+
+Fondo1 = getMask(entre1);
+Fondo2 = getMask(entre2);
+Fondo3 = getMask(entre3);
+probaFondo = classProb(Fondo1,Fondo2,Fondo3);
+
+% Obtenemos la matriz de covarianzas y el vector de 
+% medias de cada clase, concatenando cada máscara
+matrixHalo = cat(1, immultiply(entre1,double(Halo1)),...
+                    immultiply(entre1,double(Halo2)),...
+                    immultiply(entre1,double(Halo3)));
+[covHalo, meanHalo] = meanAndCov(matrixHalo);
+
+matrixPros = cat(1, immultiply(entre1,double(Pros1)),...
+                    immultiply(entre1,double(Pros2)),...
+                    immultiply(entre1,double(Pros3)));
+[covPros, meanPros] = meanAndCov(matrixPros);
+
+matrixFondo = cat(1, immultiply(entre1,double(Fondo1)),...
+                    immultiply(entre1,double(Fondo2)),...
+                    immultiply(entre1,double(Fondo3)));
+[covFondo, meanFondo] = meanAndCov(matrixFondo);
+end
